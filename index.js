1c82b067fbf0631292be16d70cb6b8a54864b1da
onze <slapexs@gmail.com>
2022-06-20 09:56:25 +0700

    Setting hbs in index page

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const hbs = require('hbs');
 
 // Router
 const routerUser = require('./routers/user');
 
 // Extension express
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname, '/views/partials');
+app.use('/static', express.static('static'));
 
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`)
